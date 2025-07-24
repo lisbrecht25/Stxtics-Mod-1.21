93f6d040cd644f82b787eadffdef64ff2fac0eb3
lisbrecht25 <lukeisbrecht25@gmail.com>
2025-07-23 23:26:12 -0400

    Initial commit

---
+++ src/main/java/net/lukeisbrecht/stxticsmod/StxticsMod.java
@@ -0,0 +1,16 @@
+package net.lukeisbrecht.stxticsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StxticsMod implements ModInitializer {
+	public static final String MOD_ID = "stxticsmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lukeisbrecht/stxticsmod/StxticsModClient.java
@@ -0,0 +1,10 @@
+package net.lukeisbrecht.stxticsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StxticsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
